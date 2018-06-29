e82c3077e994ce8985a38c7da6a61a0e375e97e8
Dan <atarisk@gmail.com>
2018-06-29 14:23:33 +0200

    updating texts,md formatting

--- component-spa/src/components/home.js
+++ component-spa/src/components/home.js
@@ -8,7 +8,7 @@ class Home extends Component {
             <div>               
                 <h1>Home page</h1>
                 <p>all content that is bordered is WEB COMPONENT</p>
-                <p>React is used regulary as single page app, was used mostly as router and for composing web component's on subpages</p>
+                <p>React is used as single page app entry point, was used mostly as router and for composing web component's on subpages</p>
                 <h2>Browser Support</h2>
                 <ul>
                     <li>Chrome (and all Chromium based browsers)</li>
